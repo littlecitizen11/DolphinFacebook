5f2a56407932d8253ff185241351c5e4f3a54ff0
LittleCitizen11 <thelittlecitizen@gmail.com>
2020-08-23 21:48:30 +0300

    add project

---
+++ DolphinFacebook/DolphinFacebook/DolphinsFacebookClient.cs
@@ -0,0 +1,32 @@
+﻿using MamaFacebook.Abstract;
+using System;
+
+namespace DolphinFacebook
+{
+    class DolphinsFacebookClient : IFacebookClient
+    {
+        public event Action<string> NewWallPost;
+        private IDisplay _displayer;
+
+
+        public DolphinsFacebookClient(IDisplay displayer)
+        {
+            _displayer = displayer;
+        }
+        public void Subscribe(IFacebookClient publisher)
+        {
+            publisher.NewWallPost += _displayer.DisplayWallPost;
+        }
+
+        public void Unsubscribe(IFacebookClient publisher)
+        {
+            publisher.NewWallPost -= _displayer.DisplayWallPost;
+        }
+
+        public void WriteNewWallPost(string wallPost)
+        {
+            NewWallPost?.Invoke(wallPost);
+        }
+
+    }
+}
---
+++ DolphinFacebook/DolphinFacebook/FacebookClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using MamaFacebook.Abstract;
+
+
+namespace DolphinFacebook
+{
+    public class FacebookClientFactory : IFacebookClientFactory
+    {
+        public IFacebookClient CreateClient(IDisplay display)
+        {
+            return new DolphinsFacebookClient(display);
+        }
+    }
+}
